c8764f8d183539b35c10abea9f6d3a9101261b70
luis <luis.canelon@eurona.com>
2026-05-13 18:16:58 +0200

    ahora si

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8480',
         changeOrigin: true,
         secure: false
       }
